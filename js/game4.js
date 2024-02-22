9e292978636f884a7b4d4ad98969da97380d31fb
jspresnell <37853087+jspresnell@users.noreply.github.com>
2024-02-22 18:16:11 -0500

    Add files via upload

---
+++ js/game4.js
@@ -0,0 +1,73 @@
+// Loader
+$(window).on('load', function(){
+  setTimeout(function(){
+    $('.loader').fadeOut();
+  }, 1500)
+})
+
+// Characters
+var pics = ["ackbar.jpg", "biggs.jpg", "boba.jpg", "bossk.jpg", "c3p0.jpg", "chewbacca.jpg", "ewok.jpg", "greedo.jpg", "han.jpg", "jabba.jpg", "jawa.jpg", "lando.jpg", "leia.jpg", "luke.jpg", "mon.jpg", "obiwan.jpg", "oola.jpg", "palpatine.jpg", "r2d2.jpg", "r5d4.jpg", "yoda.jpg", "stormtrooper.jpg", "tarkin.jpg", "vader.jpg"];
+
+//My Guess Who Character
+function myCharacter(){
+  var myPic = Math.floor(Math.random() * pics.length);
+
+  $("#me").html('<img src="img/' + pics[myPic] + ' ">');
+
+  var name = pics[myPic].substr(0, pics[myPic].lastIndexOf("."))
+  name = name.charAt(0).toUpperCase() + name.slice(1);
+  $("#name").html('You are ' + name);
+}
+
+myCharacter();
+
+
+//Game Board
+function shuffle() {
+  pics.sort(function(a, b){return 0.5 - Math.random()});
+  for (var i = 0; i < pics.length; i++) {
+    $("#gameboard").append('<div class="card"><img src="img/' + pics[i] + ' "></div>');
+  }
+}
+
+shuffle();
+
+$("#gameboard").on('click', '.card', function(){
+  $(this).toggleClass("flipped");
+});
+
+// Game Tracker
+var win = 0;
+var loss = 0;
+
+$('#score').html(win + ' - ' + loss);
+
+function updateScore(win, loss) {
+  $('#score').html(win + ' - ' + loss);
+}
+
+
+$('#win').click(function(){
+  //win+=1;
+  //updateScore(win, loss);
+  //reset();
+})
+
+$('#loss').click(function(){
+  loss+=1;
+  updateScore(win, loss);
+  reset();
+})
+
+// Reset Game Board
+
+function reset(){
+  var play = confirm('Do you want to play again?');
+  if (play == true){
+    $('#gameboard').html('');
+    myCharacter();
+    shuffle();
+  } else {
+    alert('Thanks for playing!');
+  }
+}
---
+++ js/game5.js
@@ -0,0 +1,73 @@
+// Loader
+$(window).on('load', function(){
+  setTimeout(function(){
+    $('.loader').fadeOut();
+  }, 1500)
+})
+
+// Characters
+var pics = ["aayla.jpg", "anakin.jpg", "battle droid.jpg", "c3po.jpg", "clone.jpg", "cody.jpg", "dooku.jpg", "grievous.jpg", "gunray.jpg", "jango.jpg", "jarjar.jpg", "kit.jpg", "mace.jpg", "maul.jpg", "nass.jpg", "obi-wan.jpg", "padme.jpg", "sidious.jpg", "quigon.jpg", "r2-d2.jpg", "shmi.jpg", "super battle droid.jpg", "watto.jpg", "cgi yoda.jpg"];
+
+//My Guess Who Character
+function myCharacter(){
+  var myPic = Math.floor(Math.random() * pics.length);
+
+  $("#me").html('<img src="img/' + pics[myPic] + ' ">');
+
+  var name = pics[myPic].substr(0, pics[myPic].lastIndexOf("."))
+  name = name.charAt(0).toUpperCase() + name.slice(1);
+  $("#name").html('You are ' + name);
+}
+
+myCharacter();
+
+
+//Game Board
+function shuffle() {
+  pics.sort(function(a, b){return 0.5 - Math.random()});
+  for (var i = 0; i < pics.length; i++) {
+    $("#gameboard").append('<div class="card"><img src="img/' + pics[i] + ' "></div>');
+  }
+}
+
+shuffle();
+
+$("#gameboard").on('click', '.card', function(){
+  $(this).toggleClass("flipped");
+});
+
+// Game Tracker
+var win = 0;
+var loss = 0;
+
+$('#score').html(win + ' - ' + loss);
+
+function updateScore(win, loss) {
+  $('#score').html(win + ' - ' + loss);
+}
+
+
+$('#win').click(function(){
+  //win+=1;
+  //updateScore(win, loss);
+  //reset();
+})
+
+$('#loss').click(function(){
+  loss+=1;
+  updateScore(win, loss);
+  reset();
+})
+
+// Reset Game Board
+
+function reset(){
+  var play = confirm('Do you want to play again?');
+  if (play == true){
+    $('#gameboard').html('');
+    myCharacter();
+    shuffle();
+  } else {
+    alert('Thanks for playing!');
+  }
+}
