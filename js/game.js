48b9244d4909ea618dd3e92de370fc8af8ea7f31
jspresnell <37853087+jspresnell@users.noreply.github.com>
2024-02-27 13:26:34 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,10 +1,3 @@
-// Loader
-$(window).on('load', function(){
-  setTimeout(function(){
-    $('.loader').fadeOut();
-  }, 1500)
-})
-
 // Characters
 var pics = ["beat.jpg", "nathan.jpg", "cj.jpg", "cloud.jpg", "crash.jpg", "dante.jpg", "ezio.jpg", "gordon.jpg", "hitman.jpg", "john.jpg", "kratos.jpg", "leon.jpg", "link.jpg", "luigi.jpg", "mario.jpg", "chief.jpg", "phoenix.jpg", "ratchet.jpg", "sephiroth.jpg", "snake.jpg", "sonic.jpg", "sora.jpg", "spyro.jpg", "steve.jpg"];
 
@@ -21,7 +14,6 @@ function myCharacter(){
 
 myCharacter();
 
-
 //Game Board
 function shuffle() {
   pics.sort(function(a, b){return 0.5 - Math.random()});
@@ -35,39 +27,3 @@ shuffle();
 $("#gameboard").on('click', '.card', function(){
   $(this).toggleClass("flipped");
 });
-
-// Game Tracker
-var win = 0;
-var loss = 0;
-
-$('#score').html(win + ' - ' + loss);
-
-function updateScore(win, loss) {
-  $('#score').html(win + ' - ' + loss);
-}
-
-
-$('#win').click(function(){
-  //win+=1;
-  //updateScore(win, loss);
-  //reset();
-})
-
-$('#loss').click(function(){
-  loss+=1;
-  updateScore(win, loss);
-  reset();
-})
-
-// Reset Game Board
-
-function reset(){
-  var play = confirm('Do you want to play again?');
-  if (play == true){
-    $('#gameboard').html('');
-    myCharacter();
-    shuffle();
-  } else {
-    alert('Thanks for playing!');
-  }
-}
