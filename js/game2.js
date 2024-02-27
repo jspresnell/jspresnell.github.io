5f5129871ce7b080135beeecbccf3943304b98e6
jspresnell <37853087+jspresnell@users.noreply.github.com>
2024-02-27 13:26:47 -0500

    Update game2.js

--- js/game2.js
+++ js/game2.js
@@ -1,10 +1,3 @@
-// Loader
-$(window).on('load', function(){
-  setTimeout(function(){
-    $('.loader').fadeOut();
-  }, 1500)
-})
-
 // Characters
 var pics = ["aloy.jpg", "alyx.jpg", "amyrose.jpg", "arkgirl.jpg", "bayonetta.jpg", "chell.jpg", "chunli.jpg", "elena.jpg", "ellie.jpg", "faith.jpg", "huntress.jpg", "jill.jpg", "kassandra.jpg", "lauracroft.jpg", "lightning.jpg", "maya.jpg", "peach.jpg", "robin.jpg", "rosalina.jpg", "samus.jpg", "tifa.jpg", "willow.jpg", "yuna.jpg", "zelda.jpg"];
 
@@ -35,39 +28,3 @@ shuffle();
 $("#gameboard").on('click', '.card', function(){
   $(this).toggleClass("flipped");
 });
-
-// Game Tracker
-var win = 0;
-var loss = 0;
-
-$('#score').html(win + ' - ' + loss);
-
-function updateScore(win, loss) {
-  $('#score').html(win + ' - ' + loss);
-}
-
-
-$('#win').click(function(){
-  win+=1;
-  updateScore(win, loss);
-  reset();
-})
-
-$('#loss').click(function(){
-  loss+=1;
-  updateScore(win, loss);
-  reset();
-})
-
-// Reset Game Board
-
-function reset(){
-  var play = confirm('Do you want to play again?');
-  if (play == true){
-    $('#gameboard').html('');
-    myCharacter();
-    shuffle();
-  } else {
-    alert('Thanks for playing!');
-  }
-}
